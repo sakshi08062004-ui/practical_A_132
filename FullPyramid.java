48b7a2b08b3f833f2df078fafc714a84e9c04ca7
sakshi08062004-ui <sakshi08062004@gmail.com>
2026-02-24 15:17:22 +0530

    Refactor FullPyramid to simplify loop structure

--- FullPyramid.java
+++ FullPyramid.java
@@ -1,9 +1,7 @@
 public class FullPyramid {
     public static void main(String[] args) {
-        int rows = 5;
-
-        for (int i = 1; i <= rows; i++) {
-            for (int j = 1; j <= i; j++) {
+        for(int i=1;i<=5;i++){
+            for(int j=1;j<=i;j++){
                 System.out.print("* ");
             }
             System.out.println();
