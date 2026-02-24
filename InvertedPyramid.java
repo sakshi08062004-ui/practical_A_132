5b1afd2300bb15fe89764e1fa9467436e2b17533
sakshi08062004-ui <sakshi08062004@gmail.com>
2026-02-24 15:16:33 +0530

    Update InvertedPyramid.java

--- InvertedPyramid.java
+++ InvertedPyramid.java
@@ -1,9 +1,7 @@
 public class InvertedPyramid {
     public static void main(String[] args) {
-        int rows = 6;
-
-        for (int i = rows; i >= 1; i--) {
-            for (int j = 1; j <= i; j++) {
+        for(int i=6;i>=1;i--){
+            for(int j=1;j<=i;j++){
                 System.out.print("* ");
             }
             System.out.println();
